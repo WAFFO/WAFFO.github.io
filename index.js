d87dd35a293bc2644a28c82ee0b822d94671d35a
Garrett Steffen <garrettsteffen@gmail.com>
2019-03-25 20:50:36 -0500

    More cubes..

--- index.js
+++ index.js
@@ -330,7 +330,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"./wasm/client_bg.wasm":"0bd805817d7c18671077"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"./wasm/client_bg.wasm":"04c1ce63390be2f06494"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
